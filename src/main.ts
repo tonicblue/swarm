8a7a9b532f32cfb88e5716f166b6c64824e6c63f
Matthew Parry-Wilkes <tonicblue@gmail.com>
2023-11-24 23:19:25 +0000

    some not real progress

--- src/main.ts
+++ src/main.ts
@@ -35,6 +35,17 @@ export enum Mode {
   PREVIEW = 'PREVIEW'
 }
 
+export enum Direction {
+  UP = 'UP',
+  DOWN = 'DOWN',
+  PARENT = 'PARENT',
+  NEXT = 'NEXT',
+  FIRST = 'FIRST',
+  LAST = 'LAST'
+}
+
+type Commands = { [name: string]: (...args: any[]) => any };
+
 @customElement('swarm-root')
 export class SwarmRoot extends LitElement {
   static styles = css`
@@ -79,21 +90,76 @@ export class SwarmRoot extends LitElement {
       return;
 
     const [
-      method, ...args
+      commandName, ...args
     ] = binding;
-    console.log('COMMAND', method, args);
+    console.log('COMMAND', commandName, args);
 
-    if (method === 'changeMode')
-      this.changeMode(...args);
+    const command = this.commands[commandName];
+
+    if (!command)
+      throw Error (`Invalid command ${commandName}`);
+
+    command(...args);
   }
 
   handleClick (event: MouseEvent) {
-    console.log('Node selected', event.target);
-    this.nodeSelector.node = event.target as HTMLElement;
+    const target = event.target as HTMLElement | null;
+
+    if (target)
+      this.selectNode(target);
   }
 
-  changeMode (newMode: Mode, ...args: any[]) {
-    console.log(`Changing mode from ${this.mode} to ${newMode}`);
-    this.mode = newMode;
+  commands: Commands = {
+    changeMode: (newMode: Mode, ...args: any[]) => {
+      console.log(`Changing mode from ${this.mode} to ${newMode}`, args);
+      this.mode = newMode;
+    },
+
+    moveCursor: (...directions: Direction[]) => {
+      console.log(`Move cursor ${directions}`);
+
+      // TODO: Rethink these directions to match how something like neo-tree does it.
+      //       Need to think more in terms of collapsing and expanding branches as well as the below
+      //       as well as how down can also mean step into first child if the parent branch is
+      //       expanded. This all stinks of having these motions actually control a tree-view
+      //       component.
+      for (const direction of directions) {
+        switch (direction) {
+          case (Direction.UP):
+            if (this.current.previousSibling)
+              return this.selectNode(this.current.previousSibling as HTMLElement);
+            break;
+          case (Direction.DOWN):
+            if (this.current.nextSibling)
+              return this.selectNode(this.current.nextSibling as HTMLElement);
+            break;
+          case (Direction.PARENT):
+            if (this.current.parentNode)
+              return this.selectNode(this.current.parentNode as HTMLElement);
+            break;
+          case (Direction.NEXT):
+            // TODO: This needs to recursively go up a level and look for the `nextSibling`
+            if (this.current.parentNode?.nextSibling)
+              return this.selectNode(this.current.parentNode.nextSibling as HTMLElement);
+            break;
+          case (Direction.FIRST):
+            if (this.current.parentNode?.firstChild)
+              return this.selectNode(this.current.parentNode.firstChild as HTMLElement);
+            break;
+          case (Direction.LAST):
+            if (this.current.parentNode?.lastChild)
+              return this.selectNode(this.current.parentNode.lastChild as HTMLElement);
+            break;
+          default:
+            throw new Error (`How TF am i supposed to go "${direction}"?`);
+        }
+      }
+    },
+  };
+
+  selectNode (node: HTMLElement) {
+    console.log('Node selected', node);
+    this.current = node;
+    this.nodeSelector.node = node;
   }
 }
